e9d7c4bde54e35f520738f29cda9353a78c4369b
antspting <max.verzilov32@gmail.com>
2025-10-26 21:07:46 +0500

    feat: moved all in one file

--- ConsoleApp1/run.cs
+++ ConsoleApp1/run.cs
@@ -1,4 +1,40 @@
-﻿namespace ConsoleApp1;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1;
+
+public class State(string hallway, Dictionary<char, string> rooms)
+{
+    public string Hallway { get; } = hallway;
+    public Dictionary<char, string> Rooms { get; } = rooms;
+
+    public override int GetHashCode()
+    {
+        unchecked
+        {
+            var hash = Hallway.GetHashCode();
+            foreach (var kv in Rooms)
+                hash = (hash * 31) ^ (kv.Key.GetHashCode() ^ kv.Value.GetHashCode());
+            return hash;
+        }
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is not State s)
+            return false;
+
+        if (Hallway != s.Hallway)
+            return false;
+
+        foreach (var key in Rooms.Keys)
+            if (!s.Rooms.ContainsKey(key) || s.Rooms[key] != Rooms[key])
+                return false;
+
+        return true;
+    }
+}
 
 class Program
 {
